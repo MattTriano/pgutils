457fc22f8f72884c4854cef0cbc1fe5d6ebf9e7a
matttriano <matt.triano@gmail.com>
2021-06-11 20:10:54 -0500

    fixing something from the setup.py file.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	author="Matt Triano",
 	description="A package of tools for working with my personal data warehouse.",
-	name="pg_utils",
+	name="pgutils",
 	version="0.1.0",
 	packages=find_packages(include=["pgutils", "pgutils.*"]),
 )
